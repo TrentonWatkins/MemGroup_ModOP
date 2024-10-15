b6e6f156e1f23aa8ff31e1df6e26039811a9134d
dieffenbachda26 <147652439+dieffenbachda26@users.noreply.github.com>
2024-10-15 10:33:14 -0400

    Started outlining memory structure w/ three subclasses

---
+++ mem.util/memProc.py
@@ -0,0 +1,9 @@
+# Single mem process - one of the three that would run at the same time
+
+# Fully define what memory process needs
+#   Page(s), command queue, priority, etc
+class MemoryProc:
+    def __init__(self, priority):
+        self.priority = null
+        
+
---
+++ mem.util/priProc.py
@@ -0,0 +1,1 @@
+class priProc(memProc):
