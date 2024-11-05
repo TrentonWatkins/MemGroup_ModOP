75b5f8627710505f2cb39a736ebfd3f117368849
Trenton Watkins <watkinstw26@mail.vmi.edu>
2024-11-05 10:25:32 -0500

    Update memProc.py

--- mem.util/memProc.py
+++ mem.util/memProc.py
@@ -3,7 +3,22 @@
 # Fully define what memory process needs
 #   Page(s), command queue, priority, etc
 class MemoryProc:
-    def __init__(self, priority):
-        self.priority = null
-        
+    def __init__(self,pross, priority):
+        self.priority = null;
+        if priority == 1:
+            priProc(pross)
+        if priority == 2:
+            secProc(pross)
+        else:
+            terProc(pross)
+# netCodesocets ports to listen to 99887 from storage 
+#send to process management 
+#create impliment interfaces for pages and index so subclasses can inhearet traits
 
+    #TODO:Private Heap:
+    #Python manages memory through a private heap, which is a portion of memory dedicated to the Python process.
+    # This heap contains all Python objects and data structures -Trenton
+    import heapq
+
+    #TODO:The mmap module allows you to map a file or a portion of it directly into memory.
+    # This can be useful for working with large files efficiently or for interfacing with shared memory. - Alex 
